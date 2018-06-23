b621065b9a3d74d1c9c10ef7579bd2e2ad833ad8
NEWPLAN <enwplan001@163.com>
2018-06-23 10:38:28 +0800

    update

--- main.py
+++ main.py
@@ -208,8 +208,7 @@ def batched_voca(nums=20):
     if all_nums == 0 or nums <= 0:
         return []
     
-    if nums<all_nums or nums<5:
-        print nums, all_nums
+    if all_nums < nums or nums<5:
         print ('system error in loading vocabulary, you need add more vocabulary before loading...')
     nums -= nums%5
 
